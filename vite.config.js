8a20183b516732b794e9306acd8e74ddd52bb167
tuandq3 <tuandq3@vnpay.vn>
2025-04-08 17:08:30 +0700

    Optimize build

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "/",
   plugins: [react()],
-  esbuild: {
-    loader: "jsx",
-    include: /src\/.*\.js$/,
-  },
   server: {
     port: 3000,
   },
